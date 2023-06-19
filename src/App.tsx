9df32e0e3c0c94ab3917b6302fb4138fa85a4e35
AlexandrIvanov <alexandr.1996@list.ru>
2023-06-20 00:38:51 +0400

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEvent, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [likes, setLikes] = useState(5)
+
+    const [value, setValue] = useState('Input text')
+
+    const increment = () => setLikes(state => state + 1)
+    const decrement = () => setLikes(state => state - 1)
+
+    const onInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
+
+    return (
+        <div className="App">
+            <h1>{likes}</h1>
+            <button onClick={increment}>inc</button>
+            <button onClick={decrement}>dec</button>
+            <div>
+                <input type="text" value={value} onChange={onInputHandler}/>
+            </div>
+        </div>
+    )
 }
 
 export default App;
